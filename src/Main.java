59c590aeb81d2c909f65c23fded0001da403d14c
Jdunn1998 <dunn2jm@mail.uc.edu>
2025-02-09 17:00:19 -0500

    Finished Task 3

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Variables
+        double intialBalance = 5000.00;
+        double interestRate = 0.17;
+        double firstMonthInterest = intialBalance * (interestRate / 12);
+        double secondMonthInterest = (intialBalance + firstMonthInterest) * (interestRate / 12);
+
+        //Outputs
+        System.out.println("Interest after one month: $" + firstMonthInterest);
+        System.out.println("Interest after two months: $" + secondMonthInterest);
     }
 }
